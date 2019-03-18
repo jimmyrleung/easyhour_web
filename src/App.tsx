b9e7980ffc6881b2448af4db4311df348698d832
Jimmy Rios Leung <jimmy.leung@7comm.com.br>
2019-03-18 17:25:29 -0300

    react app with ts and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Drawer from './components/menu/Drawer';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <p>Welcome to Easyhour</p>
-      </div>
-    );
+    return <Drawer title="Easyhour" />;
   }
 }
 
